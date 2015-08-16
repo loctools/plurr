cd72a9878136d9e27557ab3248710a01f4551c6a
Julen Ruiz Aizpuru <julenx@gmail.com>
2015-08-16 13:52:32 +0200

    PEP8: two spaces after inline comments

--- python/plurr.py
+++ python/plurr.py
@@ -17,152 +17,152 @@ class Plurr(object):
     # http://translate.sourceforge.net/wiki/l10n/pluralforms
     #
     _plural_equations = {
-        'ach': lambda n: 0, # Acholi
-        'af': lambda n: 1 if (n!=1) else 0, # Afrikaans
-        'ak': lambda n: 1 if (n>1) else 0, # Akan
-        'am': lambda n: 1 if (n>1) else 0, # Amharic
-        'an': lambda n: 1 if (n!=1) else 0, # Aragonese
-        'ar': lambda n: 0 if n==0 else 1 if n==1 else 2 if n==2 else 3 if n%100>=3 and n%100<=10 else 4 if n%100>=11 else 5, # Arabic
-        'arn': lambda n: 1 if (n>1) else 0, # Mapudungun
-        'ast': lambda n: 1 if (n!=1) else 0, # Asturian
-        'ay': lambda n: 0, # Aymara
-        'az': lambda n: 1 if (n!=1) else 0, # Azerbaijani
+        'ach': lambda n: 0,  # Acholi
+        'af': lambda n: 1 if (n!=1) else 0,  # Afrikaans
+        'ak': lambda n: 1 if (n>1) else 0,  # Akan
+        'am': lambda n: 1 if (n>1) else 0,  # Amharic
+        'an': lambda n: 1 if (n!=1) else 0,  # Aragonese
+        'ar': lambda n: 0 if n==0 else 1 if n==1 else 2 if n==2 else 3 if n%100>=3 and n%100<=10 else 4 if n%100>=11 else 5,  # Arabic
+        'arn': lambda n: 1 if (n>1) else 0,  # Mapudungun
+        'ast': lambda n: 1 if (n!=1) else 0,  # Asturian
+        'ay': lambda n: 0,  # Aymara
+        'az': lambda n: 1 if (n!=1) else 0,  # Azerbaijani
 
-        'be': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2, # Belarusian
-        'bg': lambda n: 1 if (n!=1) else 0, # Bulgarian
-        'bn': lambda n: 1 if (n!=1) else 0, # Bengali
-        'bo': lambda n: 0, # Tibetan
-        'br': lambda n: 1 if (n>1) else 0, # Breton
-        'bs': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2, # Bosnian
+        'be': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2,  # Belarusian
+        'bg': lambda n: 1 if (n!=1) else 0,  # Bulgarian
+        'bn': lambda n: 1 if (n!=1) else 0,  # Bengali
+        'bo': lambda n: 0,  # Tibetan
+        'br': lambda n: 1 if (n>1) else 0,  # Breton
+        'bs': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2,  # Bosnian
 
-        'ca': lambda n: 1 if (n!=1) else 0, # Catalan
-        'cgg': lambda n: 0, # Chiga
-        'cs': lambda n: 0 if (n==1) else 1 if (n>=2 and n<=4) else 2, # Czech
-        'csb': lambda n: 0 if n==1 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2, # Kashubian
-        'cy': lambda n: 0 if (n==1) else 1 if (n==2) else 2 if (n!=8 and n!=11) else 3, # Welsh
+        'ca': lambda n: 1 if (n!=1) else 0,  # Catalan
+        'cgg': lambda n: 0,  # Chiga
+        'cs': lambda n: 0 if (n==1) else 1 if (n>=2 and n<=4) else 2,  # Czech
+        'csb': lambda n: 0 if n==1 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2,  # Kashubian
+        'cy': lambda n: 0 if (n==1) else 1 if (n==2) else 2 if (n!=8 and n!=11) else 3,  # Welsh
 
-        'da': lambda n: 1 if (n!=1) else 0, # Danish
-        'de': lambda n: 1 if (n!=1) else 0, # German
-        'dz': lambda n: 0, # Dzongkha
+        'da': lambda n: 1 if (n!=1) else 0,  # Danish
+        'de': lambda n: 1 if (n!=1) else 0,  # German
+        'dz': lambda n: 0,  # Dzongkha
 
-        'el': lambda n: 1 if (n!=1) else 0, # Greek
-        'en': lambda n: 1 if (n!=1) else 0, # English
-        'eo': lambda n: 1 if (n!=1) else 0, # Esperanto
-        'es': lambda n: 1 if (n!=1) else 0, # Spanish
-        'es-ar': lambda n: 1 if (n!=1) else 0, # Argentinean Spanish
-        'et': lambda n: 1 if (n!=1) else 0, # Estonian
-        'eu': lambda n: 1 if (n!=1) else 0, # Basque
+        'el': lambda n: 1 if (n!=1) else 0,  # Greek
+        'en': lambda n: 1 if (n!=1) else 0,  # English
+        'eo': lambda n: 1 if (n!=1) else 0,  # Esperanto
+        'es': lambda n: 1 if (n!=1) else 0,  # Spanish
+        'es-ar': lambda n: 1 if (n!=1) else 0,  # Argentinean Spanish
+        'et': lambda n: 1 if (n!=1) else 0,  # Estonian
+        'eu': lambda n: 1 if (n!=1) else 0,  # Basque
 
-        'fa': lambda n: 0, # Persian
-        'fi': lambda n: 1 if (n!=1) else 0, # Finnish
-        'fil': lambda n: 1 if (n>1) else 0, # Filipino
-        'fo': lambda n: 1 if (n!=1) else 0, # Faroese
-        'fr': lambda n: 1 if (n>1) else 0, # French
-        'fur': lambda n: 1 if (n!=1) else 0, # Friulian
-        'fy': lambda n: 1 if (n!=1) else 0, # Frisian
+        'fa': lambda n: 0,  # Persian
+        'fi': lambda n: 1 if (n!=1) else 0,  # Finnish
+        'fil': lambda n: 1 if (n>1) else 0,  # Filipino
+        'fo': lambda n: 1 if (n!=1) else 0,  # Faroese
+        'fr': lambda n: 1 if (n>1) else 0,  # French
+        'fur': lambda n: 1 if (n!=1) else 0,  # Friulian
+        'fy': lambda n: 1 if (n!=1) else 0,  # Frisian
 
-        'ga': lambda n: 0 if n==1 else 1 if n==2 else 2 if n<7 else 3 if n<11 else 4, # Irish
-        'gl': lambda n: 1 if (n!=1) else 0, # Galician
-        'gu': lambda n: 1 if (n!=1) else 0, # Gujarati
-        'gun': lambda n: 1 if (n>1) else 0, # Gun
+        'ga': lambda n: 0 if n==1 else 1 if n==2 else 2 if n<7 else 3 if n<11 else 4,  # Irish
+        'gl': lambda n: 1 if (n!=1) else 0,  # Galician
+        'gu': lambda n: 1 if (n!=1) else 0,  # Gujarati
+        'gun': lambda n: 1 if (n>1) else 0,  # Gun
 
-        'ha': lambda n: 1 if (n!=1) else 0, # Hausa
-        'he': lambda n: 1 if (n!=1) else 0, # Hebrew
-        'hi': lambda n: 1 if (n!=1) else 0, # Hindi
-        'hy': lambda n: 0, # Armenian
-        'hr': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2, # Croatian
-        'hu': lambda n: 1 if (n!=1) else 0, # Hungarian
+        'ha': lambda n: 1 if (n!=1) else 0,  # Hausa
+        'he': lambda n: 1 if (n!=1) else 0,  # Hebrew
+        'hi': lambda n: 1 if (n!=1) else 0,  # Hindi
+        'hy': lambda n: 0,  # Armenian
+        'hr': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2,  # Croatian
+        'hu': lambda n: 1 if (n!=1) else 0,  # Hungarian
 
-        'ia': lambda n: 1 if (n!=1) else 0, # Interlingua
-        'id': lambda n: 0, # Indonesian
-        'is': lambda n: (n%10!=1 or n%100==11), # Icelandic
-        'it': lambda n: 1 if (n!=1) else 0, # Italian
+        'ia': lambda n: 1 if (n!=1) else 0,  # Interlingua
+        'id': lambda n: 0,  # Indonesian
+        'is': lambda n: (n%10!=1 or n%100==11),  # Icelandic
+        'it': lambda n: 1 if (n!=1) else 0,  # Italian
 
-        'ja': lambda n: 0, # Japanese
-        'jv': lambda n: 1 if (n!=0) else 0, # Javanese
+        'ja': lambda n: 0,  # Japanese
+        'jv': lambda n: 1 if (n!=0) else 0,  # Javanese
 
-        'ka': lambda n: 0, # Georgian
-        'kk': lambda n: 0, # Kazakh
-        'km': lambda n: 0, # Khmer
-        'kn': lambda n: 1 if (n!=1) else 0, # Kannada
-        'ko': lambda n: 0, # Korean
-        'ku': lambda n: 1 if (n!=1) else 0, # Kurdish
-        'kw': lambda n: 0 if (n==1) else 1 if (n==2) else 2 if (n==3) else 3, # Cornish
-        'ky': lambda n: 0, # Kyrgyz
+        'ka': lambda n: 0,  # Georgian
+        'kk': lambda n: 0,  # Kazakh
+        'km': lambda n: 0,  # Khmer
+        'kn': lambda n: 1 if (n!=1) else 0,  # Kannada
+        'ko': lambda n: 0,  # Korean
+        'ku': lambda n: 1 if (n!=1) else 0,  # Kurdish
+        'kw': lambda n: 0 if (n==1) else 1 if (n==2) else 2 if (n==3) else 3,  # Cornish
+        'ky': lambda n: 0,  # Kyrgyz
 
-        'lb': lambda n: (n!=1), # Letzeburgesch
-        'ln': lambda n: 1 if (n>1) else 0, # Lingala
-        'lo': lambda n: 0, # Lao
-        'lt': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n%10>=2 and (n%100<10 or n%100>=20) else 2, # Lithuanian
-        'lv': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n!=0 else 2, # Latvian
+        'lb': lambda n: (n!=1),  # Letzeburgesch
+        'ln': lambda n: 1 if (n>1) else 0,  # Lingala
+        'lo': lambda n: 0,  # Lao
+        'lt': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n%10>=2 and (n%100<10 or n%100>=20) else 2,  # Lithuanian
+        'lv': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n!=0 else 2,  # Latvian
 
-        'mfe': lambda n: 1 if (n>1) else 0, # Mauritian Creole
-        'mg': lambda n: 1 if (n>1) else 0, # Malagasy
-        'mi': lambda n: 1 if (n>1) else 0, # Maori
-        'mk': lambda n: 0 if n==1 or n%10==1 else 1, # Macedonian
-        'ml': lambda n: 1 if (n!=1) else 0, # Malayalam
-        'mn': lambda n: 1 if (n!=1) else 0, # Mongolian
-        'mr': lambda n: 1 if (n!=1) else 0, # Marathi
-        'ms': lambda n: 0, # Malay
-        'mt': lambda n: 0 if n==1 else 1 if n==0 or (n%100>1 and n%100<11) else 2 if (n%100>10 and n%100<20) else 3, # Maltese
+        'mfe': lambda n: 1 if (n>1) else 0,  # Mauritian Creole
+        'mg': lambda n: 1 if (n>1) else 0,  # Malagasy
+        'mi': lambda n: 1 if (n>1) else 0,  # Maori
+        'mk': lambda n: 0 if n==1 or n%10==1 else 1,  # Macedonian
+        'ml': lambda n: 1 if (n!=1) else 0,  # Malayalam
+        'mn': lambda n: 1 if (n!=1) else 0,  # Mongolian
+        'mr': lambda n: 1 if (n!=1) else 0,  # Marathi
+        'ms': lambda n: 0,  # Malay
+        'mt': lambda n: 0 if n==1 else 1 if n==0 or (n%100>1 and n%100<11) else 2 if (n%100>10 and n%100<20) else 3,  # Maltese
 
-        'nah': lambda n: 1 if (n!=1) else 0, # Nahuatl
-        'nap': lambda n: 1 if (n!=1) else 0, # Neapolitan
-        'nb': lambda n: 1 if (n!=1) else 0, # Norwegian Bokmal
-        'ne': lambda n: 1 if (n!=1) else 0, # Nepali
-        'nl': lambda n: 1 if (n!=1) else 0, # Dutch
-        'se': lambda n: 1 if (n!=1) else 0, # Northern Sami
-        'nn': lambda n: 1 if (n!=1) else 0, # Norwegian Nynorsk
-        'no': lambda n: 1 if (n!=1) else 0, # Norwegian (old code)
-        'nso': lambda n: 1 if (n!=1) else 0, # Northern Sotho
+        'nah': lambda n: 1 if (n!=1) else 0,  # Nahuatl
+        'nap': lambda n: 1 if (n!=1) else 0,  # Neapolitan
+        'nb': lambda n: 1 if (n!=1) else 0,  # Norwegian Bokmal
+        'ne': lambda n: 1 if (n!=1) else 0,  # Nepali
+        'nl': lambda n: 1 if (n!=1) else 0,  # Dutch
+        'se': lambda n: 1 if (n!=1) else 0,  # Northern Sami
+        'nn': lambda n: 1 if (n!=1) else 0,  # Norwegian Nynorsk
+        'no': lambda n: 1 if (n!=1) else 0,  # Norwegian (old code)
+        'nso': lambda n: 1 if (n!=1) else 0,  # Northern Sotho
 
-        'oc': lambda n: 1 if (n>1) else 0, # Occitan
-        'or': lambda n: 1 if (n!=1) else 0, # Oriya
+        'oc': lambda n: 1 if (n>1) else 0,  # Occitan
+        'or': lambda n: 1 if (n!=1) else 0,  # Oriya
 
-        'ps': lambda n: 1 if (n!=1) else 0, # Pashto
-        'pa': lambda n: 1 if (n!=1) else 0, # Punjabi
-        'pap': lambda n: 1 if (n!=1) else 0, # Papiamento
-        'pl': lambda n: 0 if n==1 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2, # Polish
-        'pms': lambda n: 1 if (n!=1) else 0, # Piemontese
-        'pt': lambda n: 1 if (n!=1) else 0, # Portuguese
-        'pt-br': lambda n: 1 if (n>1) else 0, # Brazilian Portuguese
+        'ps': lambda n: 1 if (n!=1) else 0,  # Pashto
+        'pa': lambda n: 1 if (n!=1) else 0,  # Punjabi
+        'pap': lambda n: 1 if (n!=1) else 0,  # Papiamento
+        'pl': lambda n: 0 if n==1 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2,  # Polish
+        'pms': lambda n: 1 if (n!=1) else 0,  # Piemontese
+        'pt': lambda n: 1 if (n!=1) else 0,  # Portuguese
+        'pt-br': lambda n: 1 if (n>1) else 0,  # Brazilian Portuguese
 
-        'rm': lambda n: (n!=1), # Romansh
-        'ro': lambda n: 0 if n==1 else 1 if (n==0 or (n%100>0 and n%100<20)) else 2, # Romanian
-        'ru': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2, # Russian
+        'rm': lambda n: (n!=1),  # Romansh
+        'ro': lambda n: 0 if n==1 else 1 if (n==0 or (n%100>0 and n%100<20)) else 2,  # Romanian
+        'ru': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2,  # Russian
 
-        'sco': lambda n: 1 if (n!=1) else 0, # Scots
-        'si': lambda n: 1 if (n!=1) else 0, # Sinhala
-        'sk': lambda n: 0 if (n==1) else 1 if (n>=2 and n<=4) else 2, # Slovak
-        'sl': lambda n: 1 if n%100==1 else 2 if n%100==2 else 3 if n%100==3 or n%100==4 else 0, # Slovenian
-        'so': lambda n: 1 if (n!=1) else 0, # Somali
-        'son': lambda n: 0, # Songhay
-        'sq': lambda n: 1 if (n!=1) else 0, # Albanian
-        'sr': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2, # Serbian
-        'su': lambda n: 0, # Sundanese
-        'sw': lambda n: 1 if (n!=1) else 0, # Swahili
-        'sv': lambda n: 1 if (n!=1) else 0, # Swedish
+        'sco': lambda n: 1 if (n!=1) else 0,  # Scots
+        'si': lambda n: 1 if (n!=1) else 0,  # Sinhala
+        'sk': lambda n: 0 if (n==1) else 1 if (n>=2 and n<=4) else 2,  # Slovak
+        'sl': lambda n: 1 if n%100==1 else 2 if n%100==2 else 3 if n%100==3 or n%100==4 else 0,  # Slovenian
+        'so': lambda n: 1 if (n!=1) else 0,  # Somali
+        'son': lambda n: 0,  # Songhay
+        'sq': lambda n: 1 if (n!=1) else 0,  # Albanian
+        'sr': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2,  # Serbian
+        'su': lambda n: 0,  # Sundanese
+        'sw': lambda n: 1 if (n!=1) else 0,  # Swahili
+        'sv': lambda n: 1 if (n!=1) else 0,  # Swedish
 
-        'ta': lambda n: 1 if (n!=1) else 0, # Tamil
-        'te': lambda n: 1 if (n!=1) else 0, # Telugu
-        'tg': lambda n: 1 if (n!=1) else 0, # Tajik
-        'ti': lambda n: 1 if (n>1) else 0, # Tigrinya
-        'th': lambda n: 0, # Thai
-        'tk': lambda n: 1 if (n!=1) else 0, # Turkmen
-        'tr': lambda n: 0, # Turkish
-        'tt': lambda n: 0, # Tatar
+        'ta': lambda n: 1 if (n!=1) else 0,  # Tamil
+        'te': lambda n: 1 if (n!=1) else 0,  # Telugu
+        'tg': lambda n: 1 if (n!=1) else 0,  # Tajik
+        'ti': lambda n: 1 if (n>1) else 0,  # Tigrinya
+        'th': lambda n: 0,  # Thai
+        'tk': lambda n: 1 if (n!=1) else 0,  # Turkmen
+        'tr': lambda n: 0,  # Turkish
+        'tt': lambda n: 0,  # Tatar
 
-        'ug': lambda n: 0, # Uyghur
-        'uk': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2, # Ukrainian
-        'ur': lambda n: 1 if (n!=1) else 0, # Urdu
-        'uz': lambda n: 0, # Uzbek
+        'ug': lambda n: 0,  # Uyghur
+        'uk': lambda n: 0 if n%10==1 and n%100!=11 else 1 if n%10>=2 and n%10<=4 and (n%100<10 or n%100>=20) else 2,  # Ukrainian
+        'ur': lambda n: 1 if (n!=1) else 0,  # Urdu
+        'uz': lambda n: 0,  # Uzbek
 
-        'vi': lambda n: 0, # Vietnamese
+        'vi': lambda n: 0,  # Vietnamese
 
-        'wa': lambda n: 1 if (n>1) else 0, # Walloon
+        'wa': lambda n: 1 if (n>1) else 0,  # Walloon
 
-        'zh': lambda n: 0, # Chinese
-        'zh-personal': lambda n: 1 if (n>1) else 0, # Chinese, used in special cases when dealing with personal pronoun
+        'zh': lambda n: 0,  # Chinese
+        'zh-personal': lambda n: 1 if (n>1) else 0,  # Chinese, used in special cases when dealing with personal pronoun
     }
 
     def __init__(self, options=None):
@@ -239,9 +239,9 @@ class Plurr(object):
 
                 if strict and (colon_pos == 0):
                     raise SyntaxError("Empty placeholder name")
-                if (colon_pos == -1): # simple placeholder
+                if (colon_pos == -1):  # simple placeholder
                     name = block
-                else: # multiple choices
+                else:  # multiple choices
                     name = block[0:colon_pos]
 
                 if name not in params:
@@ -269,9 +269,9 @@ class Plurr(object):
 
                 result = ''
 
-                if colon_pos == -1: # simple placeholder
+                if colon_pos == -1:  # simple placeholder
                     result = params[name]
-                else: # multiple choices
+                else:  # multiple choices
                     block_len = len(block)
 
                     if strict and (colon_pos == block_len - 1):
